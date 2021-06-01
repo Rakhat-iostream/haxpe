ba0e8f04f8b4997bd14a91991ec1dd508cc421bf
Stanislav Tagiev <s.tagiev@sdventures.com>
2021-06-02 00:39:06 +0300

    Change external sign in contract

--- src/Haxpe.Application.Contracts/V1/Facebook/FacebookCredentials.cs
+++ src/Haxpe.Application.Contracts/V1/Facebook/FacebookCredentials.cs
@@ -5,10 +5,8 @@ namespace Haxpe.V1.Facebook
 {
     public class FacebookCredentials
     {
-        [JsonProperty("access_token")]
         public string AccessToken { get; set; }
 
-        [JsonProperty("userID")]
         public string UserId { get; set; }
     }
 }
--- src/Haxpe.Application.Contracts/V1/Google/GoogleCredentials.cs
+++ src/Haxpe.Application.Contracts/V1/Google/GoogleCredentials.cs
@@ -4,7 +4,6 @@ namespace Haxpe.V1.Google
 {
     public class GoogleCredentials
     {
-        [JsonProperty("access_token")]
         public string AccessToken { get; set; }
     }
 }
