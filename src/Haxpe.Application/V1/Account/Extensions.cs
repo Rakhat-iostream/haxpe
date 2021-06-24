6b4bd70a9afeb263f06f54a972835711ad829a2d
Rakhat-iostream <raha0264@gmail.com>
2021-06-24 14:53:53 +0000

    Add extension

--- src/Haxpe.Application/V1/Account/Extensions.cs
+++ src/Haxpe.Application/V1/Account/Extensions.cs
@@ -1,5 +1,7 @@
 ﻿using Haxpe.Infrastructure;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,20 +22,39 @@ namespace Haxpe.V1.Account
                     {
                         case "DefaultError":
                             throw new BusinessException(HaxpeDomainErrorCodes.AccountDefaultError);
+                        case "LoginAlreadyAssociated":
+                            throw new BusinessException(HaxpeDomainErrorCodes.AccountLoginAlreadyAssociated);
                         case "InvalidEmail":
                             throw new BusinessException(HaxpeDomainErrorCodes.AccountInvalidEmail);
                         case "DuplicateEmail":
                             throw new BusinessException(HaxpeDomainErrorCodes.AccountDuplicateEmail);
+                        case "InvalidUserName":
+                            throw new BusinessException(HaxpeDomainErrorCodes.AccountInvalidUserNameOrPassword);
+                        case "DuplicateUserName":
+                            throw new BusinessException(HaxpeDomainErrorCodes.AccountDuplicateUserName);
+                        case "PasswordMismatch":
+                            throw new BusinessException(HaxpeDomainErrorCodes.AccountPasswordMismatch);
                         case "PasswordTooShort":
                             throw new BusinessException(HaxpeDomainErrorCodes.AccountPasswordTooShort);
+                        case "PasswordRequiresUniqueChars":
+                            throw new BusinessException(HaxpeDomainErrorCodes.AccountPasswordRequiresUniqueChars);
                         case "PasswordRequiresNonAlphanumeric":
                             throw new BusinessException(HaxpeDomainErrorCodes.AccountPasswordRequiresNonAlphanumeric);
                         case "PasswordRequiresDigit":
                             throw new BusinessException(HaxpeDomainErrorCodes.AccountPasswordRequiresDigit);
+                        case "PasswordRequiresLower":
+                            throw new BusinessException(HaxpeDomainErrorCodes.AccountPasswordRequiresLower);
+                        case "PasswordRequiresUpper":
+                            throw new BusinessException(HaxpeDomainErrorCodes.AccountPasswordRequiresUpper);
                         case "UserLockoutNotEnabled":
                             throw new BusinessException(HaxpeDomainErrorCodes.AccountUserLockoutNotEnabled);
                         case "InvalidToken":
                             throw new BusinessException(HaxpeDomainErrorCodes.AccountInvalidToken);
+                        case "FailedOperation":
+                            throw new BusinessException(HaxpeDomainErrorCodes.AccountFailedOperation);
+                        default:
+                            Log.Logger.Error($"Internal error code = {error.Code}");
+                            throw new BusinessException(HaxpeDomainErrorCodes.AccountDefaultError);
                     }
                 }
             }
